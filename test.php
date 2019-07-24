3bc0eaa371c2ac481cfeda6f0acc1dc17733197d
Rick Winfrey <rewinfrey@github.com>
2019-07-24 14:33:33 -0700

    Update test.php

--- test.php
+++ test.php
@@ -3,5 +3,13 @@ function writeMsg() {
     echo "Hello world!";
 }
 
+function writeMsg() {
+    echo "Hello world!";
+}
+
+function writeMsg() {
+    echo "Hello world!";
+}
+
 writeMsg(); // call the function
 ?>
