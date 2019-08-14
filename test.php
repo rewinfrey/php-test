0ca395e5706e2a0bf880e6865668aa95dfd98df2
Rick Winfrey <rewinfrey@github.com>
2019-08-14 16:30:13 -0700

    Update test.php

--- test.php
+++ test.php
@@ -6,8 +6,71 @@ function writeMsg() {
 function writeMsg() {
     echo "Hello world!";
 }
+
+function writeMsg() {
+    echo "Hello world!";
+}
+
+function writeMsg() {
+    echo "Hello world!";
+}
+
+function writeMsg() {
+    echo "Hello world!";
+}
+
+function writeMsg() {
+    echo "Hello world!";
+}
+
+function writeMsg() {
+    echo "Hello world!";
+}
+
+function writeMsg() {
+    echo "Hello world!";
+}
+
+function writeMsg() {
+    echo "Hello world!";
+}
+
+function writeMsg() {
+    echo "Hello world!";
+}
+
+function writeMsg() {
+    echo "Hello world!";
+}
+
+function writeMsg() {
+    echo "Hello world!";
+}
+
+function writeMsg() {
+    echo "Hello world!";
+}
 // quick comment
 // another comment
 
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
+writeMsg(); // call the function
 writeMsg(); // call the function
 ?>
